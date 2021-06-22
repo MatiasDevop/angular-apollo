7d4210d595fc6a603238f59ceb667614176bbf93
Nestor Matias <solomatias321@gmail.com>
2021-06-22 12:05:16 +0200

    setting apollo to use graphql

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Platform } from './models/platform';
+const Get_Platform = gql`query{
+  platform{
+    id
+    name
+  }
+}`;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  allPlatforms: Platform[] = [];
   title = 'ng-graphql';
+  /**
+   *
+   */
+  constructor(private apollo: Apollo) {
+    
+  }
+  ngOnInit(): void {
+    this.apollo.watchQuery<any>({
+      query: Get_Platform
+    }).valueChanges
+    .subscribe(({data, loading}) => {
+      console.log(loading);
+      this.allPlatforms = data.platform;
+    })
+  }
 }
