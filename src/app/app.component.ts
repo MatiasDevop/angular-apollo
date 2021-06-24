d598c1c02d3404abbc9a57ea7ed826be5090c83a
Nestor Matias <solomatias321@gmail.com>
2021-06-24 13:08:10 +0200

    mutatuins to use post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 import { Platform } from './models/platform';
 const Get_Platform = gql`query{
@@ -15,16 +15,30 @@ const Get_FilterName = gql`query ($brandName:String){
       Brand
     }
 }`;
+
+const Post_Save = gql `mutation($inputType:AddPlatformInput) {
+  addPlatform(input:$inputType){
+    platform{
+      id
+      name
+    }
+  }
+}`
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class AppComponent implements OnInit {
   
   allPlatforms: Platform[] = [];
   title = 'ng-graphql';
   selectedName: string = '';
+  platformForm: Platform = {
+    id: 0,
+    name:''
+  }
   /**
    *
    */
@@ -38,7 +52,7 @@ export class AppComponent implements OnInit {
     .subscribe(({data, loading}) => {
       console.log(loading);
       this.allPlatforms = data.platform;
-    })
+    });
   }
 
   searchByName(){
@@ -52,5 +66,33 @@ export class AppComponent implements OnInit {
       console.log(loading);
       this.allPlatforms = data.BrandFilter;
     })
+  }
+  public createdPlatform: Platform;
+  newPlatform(){
+    this.apollo.mutate({
+      mutation: Post_Save,
+      variables:{
+        inputType:{
+          name: this.platformForm.name
+        }
+      },
+      // update:(cache, {data}) => {
+      //   const existingList: any = cache.readQuery({
+      //     query: Get_Platform
+      //   });
+
+      //   const newPlatform = data
+      //}
+    }).subscribe(({data}) =>{
+      alert("Book Saved!")
+      this.allPlatforms = Object.assign([], this.allPlatforms);
+      this.allPlatforms.unshift(data["addPlatform"].platform);
+      //either you can also use push instead of unshift
+      console.log(this.allPlatforms);
+    },
+    error => {
+      console.log("there was an error sending the query", error);
+    });
+    
   }
 }
