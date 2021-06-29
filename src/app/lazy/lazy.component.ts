5779955a067bbf763c004b6750e0ce091b91dc33
Nestor Matias <solomatias321@gmail.com>
2021-06-29 12:15:50 +0200

    adding navbar and home

---
+++ src/app/lazy/lazy.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-lazy',
+    template: ``,
+    styles: [``]
+})
+export class LazyComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
